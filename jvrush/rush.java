f00ff84463442e1c810f06b4dd6a45a25cc2b06e
Artem <vlasov.art.ser@yandex.ru>
2024-07-03 20:16:59 +0300

    Добавил изменения по rush

---
+++ jvrush/rush.java
@@ -0,0 +1,22 @@
+package seminar1.jvrush;
+
+public class rush {
+    
+}
+/*Метод length() позволяет узнать длину строки – сколько в ней символов.
+ * н-р
+ * String name = "Rome";
+int count = name.length();
+ 
+ */
+/*Метод toLowerCase() позволяет преобразовать все символы строки в маленькие (строчные)
+н-р 
+String name = "ROM123";
+String name2 = name.toLowerCase()
+*/
+
+/*Метод toUpperCase() позволяет преобразовать все символы строки в большие (заглавные)
+ * н-р
+ * String name = "rom123";
+String name2 = name.toUpperCase();
+ */
