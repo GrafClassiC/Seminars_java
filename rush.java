052edcb107ea735a991604061ae60c7ca6ca6b60
GrafClassiC <vlasov.art.ser@yandex.ru>
2024-07-06 12:24:11 +0400

    добавил решение по методу сравнения содержания строки equals

--- rush.java
+++ rush.java
@@ -161,6 +161,24 @@ public class rush {
 //         }
 //     }
 // }
-}
+//}
+// 
+// public class Solution {
+//     public static void main(String[] args) {
+//         //напишите тут ваш код
+//         Scanner console = new Scanner(System.in);
+//         String a = console.next();
+//         String b = console.next();
+//         if (a.equals(b)) {
+//             System.out.println("строки одинаковые");
+//         } else {
+//         //напишите тут ваш код
+//         System.out.println("строки разные");
+//         }
+//     }
+// }
+
+
+
 
 }
