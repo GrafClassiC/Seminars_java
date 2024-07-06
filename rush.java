1c00b54d3b8c9c276b0e858ce24dc884053baf2c
GrafClassiC <vlasov.art.ser@yandex.ru>
2024-07-06 11:53:48 +0400

    Добавил решение по тернарному оператору

--- rush.java
+++ rush.java
@@ -1,5 +1,7 @@
 package seminar1;
 
+import java.util.Scanner;
+
 public class rush {
 
     /*
@@ -100,5 +102,23 @@ public class rush {
     // }
 
     // }
+    // У нас есть программа, которая считывает с клавиатуры два числа и выводит на экран 
+    // большее из них. Если числа одинаковые, то выводится любое.
+    // Перепиши программу с использованием тернарного оператора, 
+    // чтобы ее функционал остался без изменений.
+    
+    // Требования:
+    // •	Программа должна считывать два числа c клавиатуры.
+    // •	Вместо оператора if-else в программе нужно использовать тернарный оператор.
+    // •	Функционал программы не должен измениться.
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         int numberA = scanner.nextInt();
+//         int numberB = scanner.nextInt();
+        
+//         System.out.println(numberA > numberB ? numberA : numberB);
+//     }
+}
 
 }
