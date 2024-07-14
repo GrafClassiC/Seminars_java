9e5bbe169578893605d63bed9ceb709dfb25b111
Artem <vlasov.art.ser@yandex.ru>
2024-07-14 22:08:04 +0300

    Добавил решение по циклу do while вывести стрику number раз

--- Task1_Hello_World.java
+++ Task1_Hello_World.java
@@ -1,51 +1,51 @@
-package seminar1;
+// package seminar1;
 
-import java.time.LocalDateTime;
+// import java.time.LocalDateTime;
 
-// написать программу Hello World
-// вывести системную дату и время
+// // написать программу Hello World
+// // вывести системную дату и время
 
 
 
-public class Task1_Hello_World {
+// public class Task1_Hello_World {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //Date // устарел
-        //Calendar // устарел
-        //System.out.printf форматированный тип вывода
-        System.out.println(LocalDateTime.now()); // есть LocalDate и LocalTime
-        // не забывай он импортируется LocalDateTime
-        // есть форматор DateFormat форматирование даты и времени
+//     public static void main(String[] args) {
+//         System.out.println("Hello World!");
+//         //Date // устарел
+//         //Calendar // устарел
+//         //System.out.printf форматированный тип вывода
+//         System.out.println(LocalDateTime.now()); // есть LocalDate и LocalTime
+//         // не забывай он импортируется LocalDateTime
+//         // есть форматор DateFormat форматирование даты и времени
         
-    //     int n = 34534; //целочисленный тип данных
-    //     double d = 5.65; //вещественные, дробные обертка - Double для работы с дополнительным функционалом
-    //     String s = "Привет!"; //строка ссылочный тип Класс/объект
-    //     char c = 't'; // символьный тип !!!! символ указывается в одинарных ковычках
-    //     boolean b = true; //принимает либо правду либо неправду
-    //     String[] str = new String[6]; //Создали пустой(null) массив с 6-ю строками
-    //     String[] str1 = new String[]{"ere", "345", "456548"}; // создали массив с тремя строками
-    //     int[] nums = {5, 60, 7}; //Создали целочисленный массив можно иначе: int nums[] = {5, 60, 7} 
+//     //     int n = 34534; //целочисленный тип данных
+//     //     double d = 5.65; //вещественные, дробные обертка - Double для работы с дополнительным функционалом
+//     //     String s = "Привет!"; //строка ссылочный тип Класс/объект
+//     //     char c = 't'; // символьный тип !!!! символ указывается в одинарных ковычках
+//     //     boolean b = true; //принимает либо правду либо неправду
+//     //     String[] str = new String[6]; //Создали пустой(null) массив с 6-ю строками
+//     //     String[] str1 = new String[]{"ere", "345", "456548"}; // создали массив с тремя строками
+//     //     int[] nums = {5, 60, 7}; //Создали целочисленный массив можно иначе: int nums[] = {5, 60, 7} 
 
-    //     if(5 > 4){
+//     //     if(5 > 4){
         
-    //     }
-    //     while(b){
+//     //     }
+//     //     while(b){
         
-    //     }
-    //     for (int i = 0; i < nums.length; i++){ // итерация и перебор значения проход по всем элементам массива nums
+//     //     }
+//     //     for (int i = 0; i < nums.length; i++){ // итерация и перебор значения проход по всем элементам массива nums
         
-    //     } 
-    //     for (int elem : nums) { //foreach используется для прохода по коллекциям
-    //         System.out.print(elem+ " ");
-    //     }
+//     //     } 
+//     //     for (int elem : nums) { //foreach используется для прохода по коллекциям
+//     //         System.out.print(elem+ " ");
+//     //     }
         
-    //     System.out.print(name("Андрей"));
-    // }
-    // public String name(String name) {
-    //     return "Привет," + name + "!";  // передаем имя и позвращается строка - Привет "имя"
+//     //     System.out.print(name("Андрей"));
+//     // }
+//     // public String name(String name) {
+//     //     return "Привет," + name + "!";  // передаем имя и позвращается строка - Привет "имя"
 
 
-    }
+//     }
     
-}
+// }
